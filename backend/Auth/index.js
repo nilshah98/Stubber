9a51afe22c1eba41997044e81871db05a0c056dc
shivam1708 <shivam.pawase@somaiya.edu>
2020-01-29 12:26:24 +0530

    auth part 1

--- backend/Auth/index.js
+++ backend/Auth/index.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
-//const User = require('../models/user')
+const User = require('./models/user')
 
-loginRouter.post('/', async (request, response) => {
+loginRouter.post('/login', async (request, response) => {
   const body = request.body
-
+  console.log("Here");
   const user = await User.findOne({ username: body.username })
   const passwordCorrect = user === null
     ? false
-    : await bcrypt.compare(body.password, user.passwordHash)
+    : await bcrypt.compare(body.password, user.password)
 
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
@@ -22,11 +22,33 @@ loginRouter.post('/', async (request, response) => {
     id: user._id,
   }
 
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  const token = jwt.sign(userForToken, process.env.JWT_SECRET)
 
   response
     .status(200)
     .send({ token, username: user.username, name: user.name })
+})
+
+
+loginRouter.post('/signup', async (request, response) => {
+  try {
+    const body = request.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    response.json(savedUser)
+  } catch (exception) {
+    next(exception)
+  }
 })
 
 module.exports = loginRouter
