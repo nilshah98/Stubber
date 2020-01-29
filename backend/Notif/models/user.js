65b0189bf745b9125f3f396638557c0b4694a6e9
Rebecca Biju <rebecca.biju@somaiya.edu>
2020-01-30 00:34:09 +0530

    Notif mail module

---
+++ backend/Notif/models/user.js
@@ -0,0 +1,39 @@
+const mongoose=require('mongoose');
+const uniqueValidator = require("mongoose-unique-validator");
+
+const User = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: String,
+    password: String,
+    email: String,
+    phone: Number,
+    latitude: Number,
+    longitude: Number,
+    postal_address: String,
+    area: String,
+    crop: String,
+    cluster_id: Number,
+    usertype: String,
+    bank_ifsc: {
+        type: String,
+    },
+    bank_accno: {
+        type: String,
+        unique: true
+    },
+    bank_name: String,
+    razorpayLinkedAccount: String
+}).plugin(uniqueValidator)
+    .set("toJSON", {
+        transform: (doc, returnedDocument) => {
+            returnedDocument.id = returnedDocument._id.toString();
+            delete returnedDocument._id;
+            delete returnedDocument.__v;
+            delete returnedDocument.password;
+        }
+    });
+
+module.exports=mongoose.model("User", User);
