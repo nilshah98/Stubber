3a3f05f343bae9b40c8f08511ee755e2547fa793
nilshah98 <neel12@somaiya.edu>
2020-01-29 22:58:16 +0530

    sStyling added to login forms, dashboard

--- frontend/material-dashboard-react-master/src/layouts/Login.js
+++ frontend/material-dashboard-react-master/src/layouts/Login.js
@@ -13,17 +13,17 @@ import CardAvatar from "components/Card/CardAvatar.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 
-import useField from '../hooks/useField'
+import useField from "../hooks/useField";
 
 const styles = {
-    cardCategoryWhite: {
+  cardCategoryWhite: {
     color: "rgba(255,255,255,.62)",
     margin: "0",
     fontSize: "14px",
     marginTop: "0",
     marginBottom: "0"
-    },
-    cardTitleWhite: {
+  },
+  cardTitleWhite: {
     color: "#FFFFFF",
     marginTop: "0px",
     minHeight: "auto",
@@ -31,65 +31,68 @@ const styles = {
     fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
     marginBottom: "3px",
     textDecoration: "none"
-    }
+  }
 };
-  
+
 const useStyles = makeStyles(styles);
 
 export default function Login(props) {
-    const classes = useStyles()
-    const username = useField('text')
-    const password = useField('text')
+  const classes = useStyles();
+  const username = useField("text");
+  const password = useField("text");
 
-    const handleLogin = (event) => {
-        event.preventDefault()
-        console.log(username)
-        console.log(password)
-        console.log('Logged In')
-    }
+  const handleLogin = event => {
+    event.preventDefault();
+    console.log(username);
+    console.log(password);
+    console.log("Logged In");
+  };
 
-    return (
-        <div>
-            <GridContainer>
-                <GridItem xs={12} sm={12} md={8}>
-                    <Card>
-                        <CardHeader color="primary">
-                            <h4 className={classes.cardTitleWhite}>Login User</h4>
-                            <p className={classes.cardCategoryWhite}>Enter your credentials</p>
-                        </CardHeader>
-                        <CardBody>
-                            <GridContainer>
-                                <GridItem xs={12} sm={12} md={6}>
-                                    <CustomInput
-                                        labelText="Username"
-                                        id="username"
-                                        formControlProps={{
-                                        fullWidth: true
-                                        }}
-                                        inputProps={{...username}}
-                                    />
-                                </GridItem>
-                            </GridContainer>
-                            <GridContainer>
-                                <GridItem xs={12} sm={12} md={6}>
-                                    <CustomInput
-                                        labelText="Password"
-                                        id="password"
-                                        formControlProps={{
-                                        fullWidth: true
-                                        }}
-                                        inputProps={{...password}}
-                                    />
-                                </GridItem>
-                            </GridContainer>
-                        </CardBody>
-                        <CardFooter>
-                            <Button onClick = {handleLogin} color="primary">Login</Button>
-                        </CardFooter>
-                    </Card>
+  return (
+    <div>
+      <GridContainer style={{ justifyContent: "center", alignContent: "center", height: "100vh" }}>
+        <GridItem xs={12} sm={12} md={8}>
+          <Card>
+            <CardHeader color="primary">
+              <h4 className={classes.cardTitleWhite}>Login User</h4>
+              <p className={classes.cardCategoryWhite}>
+                Enter your credentials
+              </p>
+            </CardHeader>
+            <CardBody>
+              <GridContainer style={{ justifyContent: "center" }}>
+                <GridItem xs={12} sm={12} md={6}>
+                  <CustomInput
+                    labelText="Username"
+                    id="username"
+                    formControlProps={{
+                      fullWidth: true
+                    }}
+                    inputProps={{ ...username }}
+                  />
                 </GridItem>
-            </GridContainer>
-            
-        </div>
-    )
+              </GridContainer>
+              <GridContainer style={{ justifyContent: "center" }}>
+                <GridItem xs={12} sm={12} md={6}>
+                  <CustomInput
+                    labelText="Password"
+                    id="password"
+                    formControlProps={{
+                      fullWidth: true
+                    }}
+                    inputProps={{ ...password }}
+                  />
+                </GridItem>
+              </GridContainer>
+            </CardBody>
+            <CardFooter style={{ justifyContent: "center" }}>
+              <Button onClick={handleLogin} color="primary">
+                Login
+              </Button>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    </div>
+  );
 }
