9cbb7a1e8d8f72dd694e651899d630cb793a119e
parshva-b <parshva.barbhaya@somaiya.edu>
2020-01-30 13:08:16 +0530

    Localization initialized

--- frontend/material-dashboard-react-master/src/index.js
+++ frontend/material-dashboard-react-master/src/index.js
@@ -19,6 +19,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux'
 
+import './i18n'
 // core components
 import App from './app'
 import store from './store'
