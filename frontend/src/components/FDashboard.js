259b36e1ad071679e816d61a2c0f564b5e9c5ffb
shivam1708 <shivam.pawase@somaiya.edu>
2020-08-01 15:42:19 +0530

    buttons test

--- frontend/src/components/FDashboard.js
+++ frontend/src/components/FDashboard.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Button, Icon } from 'semantic-ui-react'
+
+const ButtonExampleEmphasis = () => (
+    <div>
+        <Button primary icon="cut" size="massive">
+            <Icon name="cut" />
+            Start harvesting
+        </Button>
+        <Button secondary size="massive">
+            <Icon name="history" />
+            Show History
+        </Button>
+    </div>
+);
 
 const FDashboard = () => {
     return (
         <div>
             Farmer dashboard
+            <ButtonExampleEmphasis />
         </div>
     )
 }
