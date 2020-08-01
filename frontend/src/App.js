f417395eae7c01288db6f7149d01d296ab86992d
parshva-b <parshva.barbhaya@somaiya.edu>
2020-08-01 13:45:02 +0530

    Page splits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import logo from './logo.svg'
+
+import {
+	BrowserRouter as Router,
+	Switch, Route, Link, Redirect
+} from "react-router-dom"
 
 import { useTranslation } from 'react-i18next'
+
+import FDashboard from './components/FDashboard'
+import CDashboard from './components/CDashboard'
 
 const App = () => {
 	const { t, i18n } = useTranslation()
@@ -9,8 +16,31 @@ const App = () => {
 		i18n.changeLanguage(code)
 	}
 
+	const padding = {
+		padding: 5
+	}
+
 	return (
 		<div className="App">
+			<Router>
+			<div>
+				<Link style={padding} to="/">home</Link>
+				<Link style={padding} to="/farmer">Farmer</Link>
+				<Link style={padding} to="/consumer">Consumer</Link>
+			</div>
+
+				<Switch>
+					<Route path="/farmer">
+						<FDashboard />
+					</Route>
+					<Route path="/consumer">
+						<CDashboard />
+					</Route>
+					<Route path="/">
+						<Redirect to = "/farmer" />
+					</Route>
+				</Switch>
+			</Router>
 			<h3> {t('welcome')} </h3>
 
 			<button type="button" onClick={() => changeLanguage('hi')}>
---
+++ frontend/src/components/FDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FDashboard = () => {
+    return (
+        <div>
+            Farmer dashboard
+        </div>
+    )
+}
+
+export default FDashboard
