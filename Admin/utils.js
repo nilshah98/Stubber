eca85abb8a018808dccc7737331a0d6586bd4633
nurdtechie98 <nurdtechie98@gmail.com>
2020-08-03 01:35:31 +0530

    add notify to collect stubble

--- Admin/utils.js
+++ Admin/utils.js
@@ -12,8 +12,18 @@ const calculateShare = (total_cost, convinience_charge, stubble) => {
 	return final_share;
 };
 
-const sendSms = () => {
-	console.log("send sms to everyone");
+const send_sms = (number, text) => {
+	const payload = `sender_id=FSTSMS&message=Hi Stubbers,${text}&language=english&route=p&numbers=${number}`;
+	axios
+		.post("https://www.fast2sms.com/dev/bulk", payload, {
+			headers,
+		})
+		.then((result) => {
+			console.log(result.data);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 };
 
 module.exports = { calculateShare, sendSms };
