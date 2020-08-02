73e861d3a85f525b6bab1c5e051af1ec062f2302
NightWing1998 <dhruvil.s@somaiya.edu>
2020-08-02 17:50:20 +0530

    added stubble collection registry and fixed stubble routes

--- Admin/stubble.js
+++ Admin/stubble.js
@@ -1,42 +1,48 @@
-const router = require('express').Router()
-const Stubble = require('./models/stubble')
-const utils = require('./utils')
+const router = require("express").Router();
+const Stubble = require("./models/stubble");
+const utils = require("./utils");
 
+router.post("/add", async (req, res) => {
+	const { stubbleType, number, weight } = req.body;
+	const stubble = await Stubble.findOne({ stubbleType });
+	if (stubble) {
+		stubble.farmers.push({ number, weight });
+		const result = await Stubble.findOneAndUpdate({ stubbleType }, stubble);
+		res.json(result);
+	} else {
+		const new_stubble = new Stubble({
+			stubbleType,
+			farmers: [{ number, weight }],
+		});
+		await new_stubble.save();
+		res.json(new_stubble.toJSON());
+	}
+});
 
-router.post('/add', async (req, res) => {
-  const { stubbleType, number, weight } = req.body
-  const stubble = await Stubble.findOne({ stubbleType })
-  if (stubble) {
-    stubble.farmers.push({ number, weight })
-    const result = await Stubble.findOneAndUpdate({ stubbleType }, stubble)
-    res.json(result)
-  }
-  else {
-    const new_stubble = new Stubble({ stubbleType, farmers: [{ number, weight }] })
-    await new_stubble.save()
-    res.json(result)
-  }
-})
+router.post("/calculate", async (req, res) => {
+	const { total_cost, convinience_charge, stubbleType } = req.body;
+	const stubble = await Stubble.findOne({ stubbleType });
+	const shares = utils.calculate_share(
+		total_cost,
+		convinience_charge,
+		stubble.farmers
+	);
+	utils.send_sms(shares);
+	res.send("success");
+});
 
-router.post('/calculate', async (req, res) => {
-  const { total_cost, convinience_charge, stubbleType } = req.body;
-  const stubble = await Stubble.findOne({ stubbleType })
-  const shares = utils.calculate_share(total_cost, convinience_charge, stubble.farmers)
-  utils.send_sms(shares)
-  res.send("success")
-})
+router.get("/getStubbles", async (req, res) => {
+	const stubbles = await Stubble.find();
+	allStubbles = [];
+	stubbles.forEach((stubble) => {
+		const totalWeight = stubble.farmers.reduce((accum, farmer) => {
+			return accum + farmer.weight;
+		}, 0);
+		allStubbles.push[
+			{ stubbleType: stubble.stubbleType, totalWeight, stubbleID: stubble._id }
+		];
+	});
+	res.status().json(allStubbles);
+});
 
-router.get('/getStubbles', async (req, res) => {
-  const stubbles = await Stubble.find()
-  allStubbles = []
-  stubbles.forEach(stubble => {
-    const totalWeight = stubble.farmers.reduce((accum, farmer) => {
-      return accum + farmer.weight
-    }, 0)
-    allStubbles.push[{ stubbleType:stubble.stubbleType, totalWeight, stubbleID:stubble._id }]
-  })
-  res.send(allStubbles);
-})
-
-
-module.exports = router
+module.exports = router;
--- Admin/utils.js
+++ Admin/utils.js
@@ -1,18 +1,19 @@
-
 const calculateShare = (total_cost, convinience_charge, stubble) => {
-  gross_cost = total_cost - convinience_charge;
-  total_stubble = stubble.reduce((accum, farmer) => {
-    return accum + farmer.weight
-  }, 0)
-  final_share = {}
-  stubble.forEach(farmer => {
-    final_share[farmer.number] = Math.floor((farmer.weight / total_stubble) * (gross_cost));
-  });
-  return final_share
-}
+	gross_cost = total_cost - convinience_charge;
+	total_stubble = stubble.reduce((accum, farmer) => {
+		return accum + farmer.weight;
+	}, 0);
+	final_share = {};
+	stubble.forEach((farmer) => {
+		final_share[farmer.number] = Math.floor(
+			(farmer.weight / total_stubble) * gross_cost
+		);
+	});
+	return final_share;
+};
 
 const sendSms = () => {
-  console.log("send sms to everyone")
-}
+	console.log("send sms to everyone");
+};
 
-module.exports = calculateShare,sendSms,get_weight
+module.exports = { calculateShare, sendSms };
