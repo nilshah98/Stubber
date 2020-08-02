4671357201152e0b20aa465d058d7845acd4f885
Rebecca Biju <rebecca.biju@somaiya.edu>
2020-08-02 16:02:15 +0530

    Start Auction frontend

--- frontend/src/components/FDashboard.js
+++ frontend/src/components/FDashboard.js
@@ -38,26 +38,6 @@ const ClusterDetails = () => (
 );
 
 const FDashboard = () => {
-  //   const items = [
-  //     {
-  //       header: "What does Start Harvesting mean?",
-  //       description:
-  //         "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
-  //       image:
-  //         "https://upload.wikimedia.org/wikipedia/commons/b/b0/Agriculture_in_Volgograd_Oblast_002.JPG",
-  //       //   meta: "ROI: 30%",
-  //     },
-  //     {
-  //       header: "What does Show your History mean?",
-  //       description:
-  //         "Bring to the table win-win survival strategies to ensure proactive domination.",
-  //       image:
-  //         "https://images.edrawmax.com/images/knowledge/line-graph-1-what-is.jpg",
-  //       //   meta: "ROI: 34%",
-  //     },
-  //   ];
-
-  //   const CardExampleGroupCentered = () => <Card.Group centered items={items} />;
   const imgStyle = {
     backgroundSize: "cover",
     backgroundImage:
