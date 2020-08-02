1a37a2f7f356ee380a401cc64457caa4500cceb0
parshva-b <parshva.barbhaya@somaiya.edu>
2020-08-02 15:31:25 +0530

    integration of startHarvesting + frontend for same

---
+++ frontend/src/services/farmer.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:8080/api/startHarvesting'
+
+const startHarvesting = (phone, quantity) => {
+    const farmerDeets = {
+        farmerphoneNum: phone,
+        quantity
+    }
+    const request = axios.post(baseUrl, farmerDeets)
+    console.log('sent to backend')
+    return request.then(response => response.data)
+}
+
+export default { startHarvesting }
