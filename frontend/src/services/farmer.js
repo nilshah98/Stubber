092916981c3a6a894a82460c3e108740a891dcf1
NightWing1998 <dhruvil.s@somaiya.edu>
2020-08-03 13:26:23 +0530

    Changes routes for microservices and tested them

--- frontend/src/services/farmer.js
+++ frontend/src/services/farmer.js
@@ -1,14 +1,14 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:8080/api/startHarvesting'
+import axios from "axios";
+const baseUrl = "http://localhost:8080/api/farmer/startHarvesting";
 
 const startHarvesting = (phone, quantity) => {
-    const farmerDeets = {
-        farmerphoneNum: phone,
-        quantity
-    }
-    const request = axios.post(baseUrl, farmerDeets)
-    console.log('sent to backend')
-    return request.then(response => response.data)
-}
+	const farmerDeets = {
+		farmerphoneNum: phone,
+		quantity,
+	};
+	const request = axios.post(baseUrl, farmerDeets);
+	console.log("sent to backend");
+	return request.then((response) => response.data);
+};
 
-export default { startHarvesting }
+export default { startHarvesting };
